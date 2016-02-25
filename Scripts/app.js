042107a873dc73404595e513fd4bdf1ae27e976d
Matt-Norton <Matthew.Norton@mygeorgian.ca>
2016-02-25 10:58:22 -0500

    Final Commit
    Added EventListener to log contact information to console

--- Scripts/app.js
+++ Scripts/app.js
@@ -41,7 +41,7 @@ var travelReport = (function () {
 
         Spetses.innerHTML = "This bohemian paradise is more Côte d'Azur than Grecian island. Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques. Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight.";
     }
-    
+
     function replaceAmorgos() {
         console.log("replaceAmorgos");
         var Amorgos;
@@ -58,7 +58,7 @@ var travelReport = (function () {
 
         Syros.innerHTML = "A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners. Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways.";
     }
-    
+
     function replaceMilos() {
         console.log("replaceMilos");
         var Milos;
@@ -67,7 +67,7 @@ var travelReport = (function () {
 
         Milos.innerHTML = "This volcanic island, with catacombs and ancient iron mines to explore, is dramatically rugged, and famous for its stunning rock formations. Often likened to a moonscape, it's also known for its hot springs, the ancient Venus De Milo statue that was found here, and for its diversity of incredible beaches. Known as ''the island of colours'' it's home to around 80 beaches — some only accessible by boat — ranging from stunningly white, to striking black, and even unusual red and grey. However, each beach has the same Evian-clear turquoise waters, and is surrounded by a rugged mountain landscape.";
     }
-    
+
     function replaceHydra() {
         console.log("replaceHydra");
         var Hydra;
@@ -76,7 +76,7 @@ var travelReport = (function () {
 
         Hydra.innerHTML = "The first thing you'll notice on this beautiful island is the lack of cars — and buses, motorcycles, or other wheeled vehicles, as well as high rises. The winding little streets full of beautiful 18th-century mansions, churches, cathedrals, museums and art galleries are covered in cobblestones, and mostly trafficked by donkeys and humans. Back in the day, the island attracted celebrities like Leonard Cohen and Sophia Loren, but was somehow forgotten over the years. That means that travellers to Hydra can have the chic yet almost rural island paradise more or less to themselves.";
     }
-    
+
     function replaceIthaca() {
         console.log("replaceIthaca");
         var Ithaca;
@@ -85,7 +85,7 @@ var travelReport = (function () {
 
         Ithaca.innerHTML = "Most famous for being the home of Odysseus, and a prominent figure in Homer's ''The Odyssey,'' Ithaca is said to have been inhabited since the 2nd millennium BC. Made up of two islands joined by a narrow strip of land, many visit it to see the legendary sites mentioned by Homer. However, Ithaca is more than its mythical counterpart, beckoning with secluded beaches, dramatic cliffs, beautiful olive groves, and sleepy fishing villages full of Byzantine churches and monasteries.";
     }
-    
+
     function replaceGavdos() {
         console.log("replaceGavdos");
         var Gavdos;
@@ -94,6 +94,8 @@ var travelReport = (function () {
 
         Gavdos.innerHTML = "Near the more well-known Crete, Gavdos is the most southern island in Greece — and the most southern spot in Europe discounting the Canaries. Only accessible by ferry, the remote island has only about 50 permanent residents, and can feel like your private playground. Local legend has it that the island was the home of goddess Calypso, who kept Odysseus prisoner here. Today, a favorite tourist activity is to visit the spot believed to be her cave. Be warned, you won't find any luxury hotels here. While the romantically under-developed, super laid back island has a number of rooms for rent, those are limited, as the real draw here for many is the free, seaside camping.";
     }
+
+
 
     replaceIntroParagraph()
     replaceFolegandros()
@@ -105,6 +107,27 @@ var travelReport = (function () {
     replaceIthaca()
     replaceHydra()
     replaceGavdos()
+
+    var firstname = document.getElementById("firstName");
+    
+    var lastname = document.getElementById("lastName");
+    
+    var email = document.getElementById("email");
+    
+    var comments = document.getElementById("comments");
+
+    var button = document.getElementById("button");
+
+    button.addEventListener("click", function () {
+        event.preventDefault(); 
+        // Logs all the names and values into the console on click
+        console.log("Submitted");
+        console.log(firstname.value);
+        console.log(lastname.value);
+        console.log(email.value);
+        console.log(comments.value);
+        console.log(button.value);
+    });
 
 
 })();
