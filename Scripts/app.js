859a9c4fe9eb2ed881c3fc4e8aa34dd52d2460d5
Matt-Norton <Matthew.Norton@mygeorgian.ca>
2016-02-25 09:47:48 -0500

    ReplaceParagraphs Commit 2
    More text has been injected into the code.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,10 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 var travelReport = (function () {
 
-"use strict";
-console.log("App Started...")
+    "use strict";
+    console.log("App Started...")
 
-function replaceIntroParagraph() {
+    function replaceIntroParagraph() {
         console.log("replaceIntroParagraph");
         var IntroParagraph;
 
@@ -12,8 +12,8 @@ function replaceIntroParagraph() {
 
         IntroParagraph.innerHTML = "Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. Greece has 1,400 islands, though only 230 of them are inhabited. And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.";
     }
-    
-function replaceFolegandros() {
+
+    function replaceFolegandros() {
         console.log("replaceFolegandros");
         var Folegandros;
 
@@ -21,9 +21,31 @@ function replaceFolegandros() {
 
         Folegandros.innerHTML = "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets.";
     }
+
+    function replaceAlonissos() {
+        console.log("replaceAlonissos");
+        var Alonissos;
+
+        Alonissos = document.getElementById("Alonissos");
+
+        Alonissos.innerHTML = "Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream. The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes. Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy.";
+    }
+
     
-replaceIntroParagraph()
-replaceFolegandros()
+    
+      function replaceSpetses() {
+        console.log("replaceSpetses");
+        var Spetses;
+
+        Spetses = document.getElementById("Spetses");
+
+        Spetses.innerHTML = "This bohemian paradise is more Côte d'Azur than Grecian island. Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques. Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight.";
+    }
+    
+    replaceIntroParagraph()
+    replaceFolegandros()
+    replaceAlonissos()
+    replaceSpetses()
 
 
 })();
